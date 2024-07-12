eb1e2018bef5130c35d1e217cf0fd009f1233fff
scar17off <scar17off@gmail.com>
2024-07-12 19:38:33 +0300

    Establishing the Git repository

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
